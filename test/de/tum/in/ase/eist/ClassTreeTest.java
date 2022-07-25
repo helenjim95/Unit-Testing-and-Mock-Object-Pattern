fca9a7333e2353a6c89ae90fcd0153f3e375aa6a
ge32goy <hanyin.chen@tum.de>
2022-07-25 19:34:24 +0200

    init

--- test/de/tum/in/ase/eist/ClassTreeTest.java
+++ test/de/tum/in/ase/eist/ClassTreeTest.java
@@ -1,20 +1,37 @@
 package de.tum.in.ase.eist;
 
+import org.easymock.Mock;
+import org.easymock.TestSubject;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ClassTreeTest {
+
+    @TestSubject
+    ClassTree classTree;
+
+    @Mock
+    ClassTreeElement root;
 
     // TODO: Test whether the calculation of the amount of classes is correct as described in the problem statement
     @Test
     void testClassCount() {
+        ClassTree classTree = new ClassTree(root);
+        expect(classTree.getRoot()).andReturn(root);
+        replay(root);
+        assertEquals(classTree.getClassCount(), root.calculateClassCount());
     }
 
     // TODO: Test whether the calculation of the amount of classes fails if no root is available as described in the
     // problem statement
     @Test
     void testGetClassCountThrowsOnNull() {
+        ClassTree classTree = new ClassTree(root);
+        expect(classTree.getRoot()).andReturn(null);
+        replay(root);
+        assertThrows(IllegalStateException.class, () -> classTree.getClassCount());
     }
 }
--- test/de/tum/in/ase/eist/DataManagerTest.java
+++ test/de/tum/in/ase/eist/DataManagerTest.java
@@ -6,8 +6,6 @@ import org.easymock.TestSubject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.Set;
-
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,4 +14,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class DataManagerTest {
     // TODO: Test if the creation of a class tree element works as described in the
     // problem statement
+    @TestSubject
+    DataManager dataManager;
+    @Mock
+    DataServer dataServer;
+    @Mock
+    ClassTree classTree;
+    @Mock
+    ClassTreeElement root;
+
+    @Test
+    void testCreateAtPath() {
+        ClassTree classTree = new ClassTree(root);
+        expect(classTree.getRoot()).andReturn(root);
+        replay(root);
+        replay(classTree);
+        replay(dataServer);
+        assertTrue(dataManager.createAtPath(classTree, root, "path"));
+    }
+
+
 }
