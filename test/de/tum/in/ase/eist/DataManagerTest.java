fa3baaece27c42e4d3f9f2691480a06bce705d85
Lucas Welscher <lucas.welscher@tum.de>
2022-07-25 09:51:33 +0200

    Exercise-Template pushed by Artemis

---
+++ test/de/tum/in/ase/eist/DataManagerTest.java
@@ -0,0 +1,19 @@
+package de.tum.in.ase.eist;
+
+import org.easymock.EasyMockExtension;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Set;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(EasyMockExtension.class)
+class DataManagerTest {
+    // TODO: Test if the creation of a class tree element works as described in the
+    // problem statement
+}
